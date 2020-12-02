163072a99933a8d532b7e8d19bfa3f1ca37d57f1
tom-takeru <tom.2461.ty@gmail.com>
2020-12-02 16:25:20 +0900

    merge into one file

--- videdi.py
+++ videdi.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import *
 import tkinter.font as font
+from tkinter import Frame
+from tkinter import filedialog
 import os
 import subprocess
 import threading
@@ -10,13 +12,113 @@ import speech_recognition as sr
 import sys
 APP_PATH = '/'.join(sys.argv[0].split('/')[:-3])
 # python videdi.pyで実行する時のため
-if APP_PATH == "":
+if APP_PATH == '':
     APP_PATH = '/Applications/videdi.app'
+
+class ClassFrame(Frame):
+    def __init__(self, master, bg=None, width=None, height=None):
+        super().__init__(master, bg=bg, width=width, height=height)
+
+
+class ScrollFrame(ClassFrame):
+    def __init__(self, master, log_max, bg=None, width=None, height=None):
+        super(ScrollFrame, self).__init__(master, bg=bg, width=width, height=height)
+
+        self.height = height
+        self.width = width
+        self.bg = bg
+
+        # スクロールバーの作成
+        self.scroll_bar = tk.Scrollbar(self, orient=tk.VERTICAL)
+        self.scroll_bar.pack(fill=tk.Y, side=tk.RIGHT, expand=False)
+        self.canvas = tk.Canvas(self, bg=self.bg, yscrollcommand=self.scroll_bar.set)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.scroll_bar.config(command=self.canvas.yview)
+
+        # ビューをリセット
+        self.canvas.xview_moveto(0)
+        self.canvas.yview_moveto(0)
+
+        self.interior = Frame(self.canvas, bg=self.bg, borderwidth=10)
+        self.interior_id = self.canvas.create_window(0, 0, window=self.interior, anchor=tk.NW)
+
+        self.interior.bind('<Configure>', self.configure_interior)
+        self.canvas.bind('<Configure>', self.configure_canvas)
+
+        # ログを初期化
+        self.log_max = log_max
+        self.log_vars = []
+        self.log_labels = []
+        self.log_color = 0
+        self.reset_all_logs()
+
+    def configure_interior(self, event=None):
+        size = (self.interior.winfo_reqwidth(), self.interior.winfo_reqheight())
+        self.canvas.config(scrollregion='0 0 %s %s' % size)
+        if self.interior.winfo_reqwidth() != self.canvas.winfo_width():
+            self.canvas.config(width=self.interior.winfo_reqwidth())
+
+    def configure_canvas(self, event=None):
+        if self.interior.winfo_reqwidth() != self.canvas.winfo_width():
+            self.canvas.itemconfigure(self.interior_id, width=self.canvas.winfo_width())
+
+    def mouse_y_scroll(self, event):
+        if event.delta > 0:
+            self.canvas.yview_scroll(-1, 'units')
+        elif event.delta < 0:
+            self.canvas.yview_scroll(1, 'units')
+
+    # ログのセット
+    def set_log(self, s):
+        if len(self.log_vars) < self.log_max:
+            self.log_vars.append(tk.StringVar())
+            for i in range(len(self.log_vars)-1, 0, -1):
+                self.log_vars[i].set(self.log_vars[i-1].get())
+            self.log_labels.append(tk.Label(self.interior, textvariable=self.log_vars[len(self.log_vars) - 1],
+                                            foreground='#' + format(self.log_color, '02x')*3, bg=self.bg))
+            self.log_color += int(255 / self.log_max - 1)
+            self.log_labels[len(self.log_vars)-1].pack(anchor=tk.S, fill=tk.X, padx=0, pady=0, ipadx=0, ipady=0)
+            self.log_labels[len(self.log_vars)-1].bind('<MouseWheel>', self.mouse_y_scroll)
+            self.log_vars[0].set(s)
+        else:
+            for i in range(len(self.log_vars)-1, 0, -1):
+                self.log_vars[i].set(self.log_vars[i-1].get())
+            self.log_vars[0].set(s)
+        return
+
+    # 大きいログのセット
+    def set_big_log(self, s):
+        self.set_log('')
+        self.set_log('----------------------------------------------------------')
+        self.set_log(s)
+        self.set_log('----------------------------------------------------------')
+        return
+
+    # ログのリセット
+    def reset_all_logs(self):
+        for i in range(len(self.log_labels)):
+            self.log_labels[i].destroy()
+        self.log_vars = []
+        self.log_labels = []
+        self.log_color = 0
+        # --       --     ----     ----      -----    ----     ----
+        #  \\     //     -  -    ||   \\    ||---    ||   \\   _  _
+        #   \\   //      | |     ||   ||    ||__|    ||   ||   ||
+        #    \\ //      -  -     ||  //     ||___    ||  //   _  _
+        #     \_/      ----     ----       -----    ----     ----
+        for i in range(2):
+            self.set_log('')
+        self.set_log(r'       \_/          ----      ----        -----      ----          ----')
+        self.set_log(r'     \ \ / /         _  _     | |___/ /     | |___     | |___/ /       _  _')
+        self.set_log(r'   \ \     / /        | |      | |      | |   | |___|     | |      | |      | |')
+        self.set_log(r' \ \        / /      -  -     | |---\ \    | |---     | |--- \ \     -  -')
+        self.set_log(r'--          --     ----     ----        -----     ----          ----')
+        self.set_log('')
+        return
+
 
 class Videdi:
     def __init__(self, width=800, height=500):
-        self.utility = videdi_utility.utility()
-
         # TKクラスをインスタンス化
         self.root = tk.Tk()
         # ウィンドウのタイトルを設定
@@ -73,7 +175,7 @@ class Videdi:
         self.sf_button_pos_y = pos_y + height + 15
         self.sf_button_height = 25
         self.sf_button_relwidth = 0.2
-        self.select_folder_button = tk.Button(text='フォルダの選択', command=self.select_folder_button,
+        self.select_folder_button = tk.Button(text='フォルダの選択', command=self.select_folder,
                                               font=button_font,
                                               highlightbackground=button_background, fg='black', highlightthickness=0)
         self.select_folder_button.place(relx=(1 - self.sf_button_relwidth) / 2, y=self.sf_button_pos_y,
@@ -84,7 +186,7 @@ class Videdi:
         self.scroll_pos_y = self.sf_button_pos_y + self.sf_button_height + 15
         self.scroll_height = 200
         self.log_max = 100
-        self.frame = videdi_log.LogFrame(master=self.root, log_max=self.log_max, bg=window_bg,
+        self.frame = ScrollFrame(master=self.root, log_max=self.log_max, bg=window_bg,
                                  width=self.window_width, height=self.window_height)
         self.frame.place(x=0, y=self.scroll_pos_y, relwidth=1.0, height=self.scroll_height)
         self.frame.interior.bind('<ButtonPress-1>', self.move_start)
@@ -143,6 +245,8 @@ class Videdi:
                                        relwidth=self.as_button_relwidth, height=self.as_button_height)
         self.add_subtitle_button.configure(state='disabled')
 
+
+
         # メインループでイベント待ち
         if __name__ == '__main__':
             self.root.mainloop()
@@ -158,6 +262,62 @@ class Videdi:
             self.frame.canvas.yview_scroll(-1, 'units')
         elif event.delta < 0:
             self.frame.canvas.yview_scroll(1, 'units')
+
+    # フォルダ選択処理
+    def select_folder(self):
+        folder = self.folder_dir
+        if len(folder) == 0:
+            idir = os.path.abspath(os.path.dirname(__file__))
+        else:
+            idir = os.path.abspath(os.path.dirname(folder))
+        # ジャンプカット動画作成・音声テキスト作成・字幕付き動画作成ボタン非表示化
+        self.jumpcut_button.configure(state='disabled')
+        self.speech_to_text_button.configure(state='disabled')
+        self.add_subtitle_button.configure(state='disabled')
+
+        self.folder_dir = filedialog.askdirectory(initialdir=idir)
+        if len(self.folder_dir) == 0:
+            self.folder_dir = folder
+        if os.path.exists(self.folder_dir):
+            if len(self.folder_dir) > self.folder_name_min:
+                s = self.folder_dir.split('/')
+                folder_name = s[-1]
+            else:
+                folder_name = self.folder_dir
+            if self.search_videos(self.folder_dir) != []:
+                self.current_folder_var.set(folder_name + 'フォルダを選択中')
+                # ジャンプカット動画作成・音声テキスト作成・字幕付き動画作成ボタン表示
+                self.jumpcut_button.configure(state='normal')
+                self.speech_to_text_button.configure(state='normal')
+                self.add_subtitle_button.configure(state='normal')
+            else:
+                self.current_folder_var.set(folder_name + 'フォルダには処理できる動画ファイルがありません。')
+        else:
+            self.current_folder_var.set('フォルダが選択されていません。')
+        return
+
+    # 指定フォルダ内のvideoファイル名取得
+    def search_videos(self, search_dir):
+        files = os.listdir(search_dir)
+        files = [i for i in files if i[-4:].lower() == '.mov' or i[-4:].lower() == '.mp4']
+        return sorted(files)
+
+    # フォルダを作成
+    def make_folder(self, s):
+        if os.path.exists('./' + s):
+            i = 2
+            while True:
+                if not (os.path.exists('./' + s + str(i))):
+                    new_folder = './' + s + str(i)
+                    os.mkdir('./' + s + str(i))
+                    break
+                i += 1
+        else:
+            new_folder = './' + s
+            os.mkdir('./' + s)
+
+        self.frame.set_log(new_folder.split('/')[-1] + 'フォルダ作成')
+        return new_folder
 
     # ボタン非表示化
     def hide_all_button(self):
@@ -177,19 +337,146 @@ class Videdi:
         self.add_subtitle_button.configure(state='normal')
         return
 
-    # フォルダ選択ボタンの処理
-    def select_folder_button(self):
-        self.utility.select_folder(self)
-        return
-
     # ジャンプカット動画作成ボタンの処理
     def jumpcut_button(self):
+
         # ボタン非表示化
         self.hide_all_button()
+
         # 自動カット処理をスレッディング
-        self.thread = threading.Thread(target=videdi_jumpcut.jumpcut, args=(self, ))
+        self.thread = threading.Thread(target=self.jumpcut)
         self.thread.start()
         return
+
+    # フォルダ内の動画をジャンプカット
+    def jumpcut(self):
+        # ジャンプカット開始
+        self.frame.set_big_log(self.folder_dir.split('/')[-1] + 'フォルダ内の動画のジャンプカット動画作成開始')
+        os.chdir(self.folder_dir)
+        video_dir = os.path.abspath(self.folder_dir)
+        video_list = self.search_videos(self.folder_dir)
+        for i, video in enumerate(video_list):
+            self.frame.set_log('')
+            self.frame.set_log(video + 'のジャンプカット動画作成開始 ' + str(i+1) + '/' + str(len(video_list)))
+            cut_sections = self.silence_sections(video)
+            print('\ncut_sections')
+            print(cut_sections)
+            if len(cut_sections) == 0:
+                self.frame.set_log(video + 'には無音部分がありませんでした。')
+                continue
+            video_sections = self.leave_sections(cut_sections, video)
+            print('\nleave_sections')
+            print(video_sections)
+            video_sections = self.arrange_sections(video_sections, self.min_time, self.margin_time)
+            print('\narrange_sections')
+            print(video_sections)
+            self.cut_video(video_dir, video_sections, video)
+            self.frame.set_log(video + 'のジャンプカット動画作成完了')
+
+        # ボタンを再表示
+        self.put_all_button()
+
+        # ジャンプカット完了ログ
+        self.frame.set_big_log(self.folder_dir.split('/')[-1] + 'フォルダ内の動画のジャンプカット動画作成完了')
+        return
+
+    # 無音部分検出
+    def silence_sections(self, video):
+        try:
+            self.frame.set_log(video + ' : 無音部分検知中')
+            output = subprocess.run([APP_PATH + '/Contents/MacOS/ffmpeg', '-i', video, '-af',
+                                     'silencedetect=noise=-30dB:d=0.3', '-f', 'null', '-'],
+                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except Exception as e:
+            print(e)
+            self.frame.set_log('error001')
+            return
+
+        s = str(output)
+        lines = s.split('\\n')
+        time_list = []
+        for line in lines:
+            if 'silencedetect' in line:
+                words = line.split(' ')
+                for i in range(len(words)):
+                    if 'silence_start' in words[i]:
+                        time_list.append(float(words[i + 1]))
+                    if 'silence_end' in words[i]:
+                        time_list.append(float(words[i + 1]))
+        silence_section_list = list(zip(*[iter(time_list)] * 2))
+        return silence_section_list
+
+    # カット部分のsectionsをカットしない部分のsectionsに変換
+    def leave_sections(self, sections, video):
+        try:
+            duration = 0
+            output = subprocess.run(
+                [APP_PATH + '/Contents/MacOS/ffprobe', video, '-hide_banner', '-show_format'],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            s = str(output)
+            lines = s.split('\\n')
+            for line in lines:
+                words = line.split('=')
+                if words[0] == 'duration':
+                    duration = float(words[1])
+                    break
+            time_list = []
+            if sections[0][0] == 0.0:
+                for i in range(len(sections)-1):
+                    time_list.append(sections[i][1])
+                    time_list.append(sections[i+1][0])
+                if sections[-1][1] < duration:
+                    time_list.append(sections[-1][1])
+                    time_list.append(duration)
+            else:
+                time_list.append(float(0.0))
+                time_list.append(sections[0][0])
+                for i in range(len(sections)-1):
+                    time_list.append(sections[i][1])
+                    time_list.append(sections[i+1][0])
+                if sections[-1][1] < duration:
+                    time_list.append(sections[-1][1])
+                    time_list.append(duration)
+        except Exception as e:
+            print(e)
+            self.frame.set_log('error002')
+            return
+
+        new_sections = list(zip(*[iter(time_list)] * 2))
+        return new_sections
+
+    # 間隔の短い動画部分を削除
+    def arrange_sections(self, sections, min_time, margin_time):
+        new_sections = []
+        if sections[0][0] < margin_time and (sections[0][1] - sections[0][1]) >= min_time:
+            sections[0][0] += margin_time
+        for i in range(len(sections)):
+            if (sections[i][1] - sections[i][0]) < min_time:
+                continue
+            else:
+                new_sections.append([sections[i][0] - margin_time, sections[i][1] + margin_time])
+        try:
+            new_sections[-1][1] -= margin_time
+        except Exception as e:
+            print(e)
+            self.frame.set_log('error003')
+        return new_sections
+
+    # 音のある部分を出力
+    def cut_video(self, video_dir, sections, video):
+        os.chdir(video_dir)
+        digit = len(str(len(sections)))
+        video_name = video.split('.')[0]
+        jumpcut_folder = self.make_folder(video_name + '_jumpcut')
+        for i in range(len(sections)):
+            split_file = jumpcut_folder + '/' + video_name + '_' + format(i+1, '0>' + str(digit)) + '.mp4'
+            subprocess.run(
+                [APP_PATH + '/Contents/MacOS/ffmpeg', '-i', video, '-ss', str(sections[i][0]), '-t',
+                 str(sections[i][1] - sections[i][0]), split_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # logを表示
+            if int((i+1)*100/len(sections)) != int(i*100/len(sections)):
+                self.frame.set_log(video + '   ' + str(int(((i+1) * 100) / len(sections))) + '%完了')
+        return jumpcut_folder
 
     # 音声テキスト作成ボタンの処理
     def speech_to_text_button(self):
@@ -204,7 +491,7 @@ class Videdi:
     def speech_to_text(self):
         # 音声テキスト作成開始ログ
         self.frame.set_big_log(self.folder_dir.split('/')[-1] + 'フォルダ内の動画の音声テキスト作成開始')
-        video_list = self.utility.search_videos(self.folder_dir)
+        video_list = self.search_videos(self.folder_dir)
         # 音声認識処理
         self.speech_recognize(self.folder_dir, video_list)
         # ボタンを再表示
@@ -216,7 +503,7 @@ class Videdi:
     # 音声認識処理
     def speech_recognize(self, video_dir, video_list):
         os.chdir(video_dir)
-        text_folder = self.utility.make_folder(self, video_dir.split('/')[-1] + '_sub')
+        text_folder = self.make_folder(video_dir.split('/')[-1] + '_sub')
         os.mkdir('.tmp')
         for i, video in enumerate(video_list):
             try:
@@ -253,18 +540,18 @@ class Videdi:
         # 音声テキスト作成開始ログ
         self.frame.set_big_log(self.folder_dir.split('/')[-1] + 'フォルダ内の動画の字幕付き動画作成開始')
         os.chdir(self.folder_dir)
-        video_list = self.utility.search_videos(self.folder_dir)
+        video_list = self.search_videos(self.folder_dir)
         os.mkdir('.tmp')
         for i, video in enumerate(video_list):
             self.frame.set_log(video + 'の字幕付き動画作成開始 ' + str(i+1) + '/' + str(len(video_list)))
             shutil.copyfile(video, '.tmp/' + video)
-            cut_sections = videdi_jumpcut.silence_sections(self, video)
-            video_sections = videdi_jumpcut.leave_sections(self, cut_sections, video)
-            video_sections = videdi_jumpcut.arrange_sections(self, video_sections, self.min_time, self.margin_time)
+            cut_sections = self.silence_sections(video)
+            video_sections = self.leave_sections(cut_sections, video)
+            video_sections = self.arrange_sections(video_sections, self.min_time, self.margin_time)
             self.frame.set_log(video + 'の音声認識のために動画をカット')
             shutil.copyfile(video, '.tmp/' + video)
-            jumpcut_folder = videdi_jumpcut.cut_video(self, self.folder_dir + '/.tmp', video_sections, video)
-            jumpcut_video_list = self.utility.search_videos(jumpcut_folder)
+            jumpcut_folder = self.cut_video(self.folder_dir + '/.tmp', video_sections, video)
+            jumpcut_video_list = self.search_videos(jumpcut_folder)
             self.speech_recognize(jumpcut_folder, jumpcut_video_list)
             texts_path = './.tmp/' + jumpcut_folder.split('/')[-1] + '/' + jumpcut_folder.split('/')[-1] + '_sub'
             self.make_srt(self.folder_dir, texts_path, video, video_sections)
@@ -323,8 +610,4 @@ def main():
     Videdi()
 
 if __name__ == '__main__':
-    # other files
-    import videdi_log
-    import videdi_jumpcut
-    import videdi_utility
     main()
