290f6cf55526a8e90cb568773e072f96cc1cdd85
tom-takeru <tom.2461.ty@gmail.com>
2020-12-22 12:55:23 +0900

    divide into videdi.py and video_audio_player.py

--- videdi.py
+++ videdi.py
@@ -1,155 +1,51 @@
-import tkinter as tk
-from tkinter import *
-import tkinter.font as font
-from tkinter import Frame
-from tkinter import filedialog
 import os
+import sys
 import subprocess
 import threading
-import shutil
-import speech_recognition as sr
-import time
-import imageio
-from PIL import ImageTk, Image
-from imageio.plugins.ffmpeg import FfmpegFormat
-import simpleaudio
-
 import io
-import sys
+import shutil
+import tkinter as tk
+from tkinter import font
+from tkinter import filedialog
+import speech_recognition as sr
 
+# 自作モジュール
+import video_audio_player
+
+# 出力のエンコードをUTF-8に設定
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-
+# 実行ファイルの絶対パスを変数に入れる
 APP_PATH = '/'.join(sys.argv[0].split('/')[:-3])
-# python videdi.pyで実行する時のため
+# python videdi.pyで実行する場合
 if APP_PATH == '':
     APP_PATH = '/Applications/videdi.app'
 FFMPEG_PATH = APP_PATH + '/Contents/MacOS/ffmpeg'
 FFPROBE_PATH = APP_PATH + '/Contents/MacOS/ffprobe'
 
-# ----------------------ファイル分割できそう(ここから)------------------------
-# 音楽を再生する
-class Audio_player():
-    def __init__(self):
-        pass
-    def __del__(self):
-        try:
-            os.remove(self.audio)
-        except:
-            pass
-    def openfile(self, file_path):
-        self.audio = file_path.split('.')[0] + '.wav'
-        if os.path.exists(self.audio):
-            os.remove(self.audio)
-        try:
-            command = [FFMPEG_PATH, '-i', file_path, self.audio]
-            subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        except Exception as e:
-            print(e)
-    def play(self):
-        wav_obj = simpleaudio.WaveObject.from_wave_file(self.audio)
-        self.play_obj = wav_obj.play()
-        self.play_obj.wait_done()
-    def stop(self):
-        try:
-            self.play_obj.stop()
-        except:
-            pass
-
-class Video_player():
-    def __init__(self):
-        format = FfmpegFormat(
-            "ffmpeg",
-            "Many video formats and cameras (via ffmpeg)",
-            ".mov .avi .mpg .mpeg .mp4 .mkv .wmv .webm",
-            "I",
-            )
-        imageio.formats.add_format(format,True)
-        self.stop_bln = False
-    def openfile(self, file_path,frame):
-        self.frame = frame
-        try:
-            self.video = imageio.get_reader(file_path)
-        except imageio.core.fetching.NeedDownloadError:
-            imageio.plugins.avbin.download()
-            self.video = imageio.get_reader(file_path)
-    def play(self):
-        self.video_thread = threading.Thread(target=self._stream)
-        self.video_thread.start()
-    def stop(self):
-        try:
-            self.stop_bln = True
-        except Exception as e:
-            print('error:Video_player.stop method')
-            print(e)
-            pass
-        return
-    def _stream(self):
-        # print(self.video.get_meta_data())
-        start_time=time.time()
-        sleeptime = 1/self.video.get_meta_data()["fps"]
-        frame_now = 0
-        for image in self.video.iter_data():
-            if self.stop_bln:
-                break
-            try:
-                frame_now = frame_now + 1
-                if frame_now * sleeptime >= time.time() - start_time:
-                    img = Image.fromarray(image).resize((670, 490))
-                    frame_image = ImageTk.PhotoImage(img)
-                    self.frame.config(image=frame_image)
-                    self.frame.image = frame_image
-                    time.sleep(sleeptime)
-                else:
-                    pass
-            except:
-                pass
-        return
-
-class Video_Audio_player():
-    def __init__(self):
-        self.video_player = Video_player()
-        self.audio_player = Audio_player()
-    def openfile(self, file_path, frame):
-        self.video_player.openfile(file_path, frame)
-        self.audio_player.openfile(file_path)
-    def play(self):
-        self.video_player.play()
-        self.audio_player.play()
-    def stop(self):
-        self.video_player.stop()
-        self.audio_player.stop()
-
-# ---------------------------ファイル分割できそう(ここまで)------------------------
-
-class ClassFrame(Frame):
+class ClassFrame(tk.Frame):
     def __init__(self, master, bg=None, width=None, height=None):
         super().__init__(master, bg=bg, width=width, height=height)
 
 class ScrollFrame(ClassFrame):
     def __init__(self, master, log_max, bg=None, width=None, height=None):
         super(ScrollFrame, self).__init__(master, bg=bg, width=width, height=height)
-
+        # スクロールバーの大きさと背景を設定
         self.height = height
         self.width = width
         self.bg = bg
-
         # スクロールバーの作成
         self.scroll_bar = tk.Scrollbar(self, orient=tk.VERTICAL)
         self.scroll_bar.pack(fill=tk.Y, side=tk.RIGHT, expand=False)
         self.canvas = tk.Canvas(self, bg=self.bg, yscrollcommand=self.scroll_bar.set)
         self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scroll_bar.config(command=self.canvas.yview)
-
         # ビューをリセット
         self.canvas.xview_moveto(0)
         self.canvas.yview_moveto(0)
-
-        self.interior = Frame(self.canvas, bg=self.bg, borderwidth=10)
+        self.interior = tk.Frame(self.canvas, bg=self.bg, borderwidth=10)
         self.interior_id = self.canvas.create_window(0, 0, window=self.interior, anchor=tk.NW)
-
         self.interior.bind('<Configure>', self.configure_interior)
         self.canvas.bind('<Configure>', self.configure_canvas)
-
         # ログを初期化
         self.log_max = log_max
         self.log_vars = []
@@ -174,22 +70,30 @@ class ScrollFrame(ClassFrame):
             self.canvas.yview_scroll(1, 'units')
 
     # ログのセット
-    def set_log(self, s):
+    def set_log(self, log_text):
+        # ログの行数が最大値未満の場合
         if len(self.log_vars) < self.log_max:
+            # ログのテキストを入れる変数を追加する
             self.log_vars.append(tk.StringVar())
+            # ログを一つずつ後ろにずらす
             for i in range(len(self.log_vars)-1, 0, -1):
                 self.log_vars[i].set(self.log_vars[i-1].get())
+            # ログの色を設定する
+            self.log_color += int(255 / self.log_max - 1)
+            # ログを表示するラベルを追加する
             self.log_labels.append(tk.Label(self.interior, textvariable=self.log_vars[len(self.log_vars) - 1],
                                             foreground='#' + format(self.log_color, '02x')*3, bg=self.bg))
-            self.log_color += int(255 / self.log_max - 1)
+            # ログのラベルをホイールで動くようにする
             self.log_labels[len(self.log_vars)-1].pack(anchor=tk.S, fill=tk.X, padx=0, pady=0, ipadx=0, ipady=0)
             self.log_labels[len(self.log_vars)-1].bind('<MouseWheel>', self.mouse_y_scroll)
-            self.log_vars[0].set(s)
+            # テキストを設定する
+            self.log_vars[0].set(log_text)
         else:
+            # ログを一つずつ後ろにずらす
             for i in range(len(self.log_vars)-1, 0, -1):
                 self.log_vars[i].set(self.log_vars[i-1].get())
-            self.log_vars[0].set(s)
-        return
+            # テキストをセットする
+            self.log_vars[0].set(log_text)
 
     # 大きいログのセット
     def set_big_log(self, s):
@@ -231,38 +135,31 @@ class Videdi:
         # ウィンドウの大きさを設定
         self.window_width = width
         self.window_height = height
-
         # ウィンドウをど真ん中に設定
         # self.window_pos_x = (self.root.winfo_screenwidth() - self.window_width) // 2
         # self.window_pos_y = (self.root.winfo_screenheight() - self.window_height) // 2
-
         # ウィンドウを左上に設定
         self.window_pos_x = 0
         self.window_pos_y = 0
-
         # ウィンドウの大きさを指定
         self.root.geometry(str(self.window_width) + 'x' + str(self.window_height)
                            + '+' + str(self.window_pos_x) + '+' + str(self.window_pos_y))
-
         # ウィンドウの背景の色を設定
         window_bg = '#ececec'
         self.root.configure(bg=window_bg)
         # ウィンドウの枠を指定
-        self.root.configure(borderwidth=10, relief=RIDGE)
-
+        self.root.configure(borderwidth=10, relief=tk.RIDGE)
         # フォントを指定
         bold_font = font.Font(self.root, size=16, weight='bold')
         button_font = font.Font(self.root, size=14)
         process_button_fg = 'red'
         self.button_background = window_bg
-
         # タイトルラベル
         pos_y = 0
         self.title_height = 100
         title_font = font.Font(self.root, family='impact', size=self.title_height, weight='bold')
         self.title_lab = tk.Label(text='VIDEDI', font=title_font, foreground='lightgray', bg='black')
         self.title_lab.place(x=0, y=pos_y, relwidth=1.0, height=self.title_height)
-
         # 現在選択中フォルダラベル
         pos_y += self.title_height + 15
         height = 20
@@ -272,7 +169,6 @@ class Videdi:
         self.current_dir_var.set('編集したい動画のあるフォルダを選択してください')
         self.current_dir_lab = tk.Label(textvariable=self.current_dir_var, font=bold_font, bg=window_bg)
         self.current_dir_lab.place(x=0, y=pos_y, relwidth=1.0, height=height)
-
         # フォルダ選択ボタン
         self.dir_is_available = False
         self.sd_button_pos_y = pos_y + height + 15
@@ -283,7 +179,6 @@ class Videdi:
                                               highlightbackground=self.button_background, fg='black', highlightthickness=0)
         self.select_dir_button.place(relx=(1 - self.sd_button_relwidth) / 2, y=self.sd_button_pos_y,
                                         relwidth=self.sd_button_relwidth, height=self.sd_button_height)
-
         # スクロール式のログラベル
         self.scroll_pos_y = self.sd_button_pos_y + self.sd_button_height + 15
         self.scroll_height = 200
@@ -294,7 +189,6 @@ class Videdi:
         self.log_frame.interior.bind('<ButtonPress-1>', self.move_start)
         self.log_frame.interior.bind('<B1-Motion>', self.move_move)
         self.log_frame.interior.bind('<MouseWheel>', self.mouse_y_scroll)
-
         # ログリセットボタン
         self.lr_button_relx = 0.85
         self.lr_button_pos_y = self.sd_button_pos_y + 15
@@ -305,12 +199,10 @@ class Videdi:
                                           highlightbackground=self.button_background, fg='black', highlightthickness=0)
         self.log_reset_button.place(relx=self.lr_button_relx, y=self.lr_button_pos_y,
                                     relwidth=self.lr_button_relwidth, height=self.lr_button_height)
-
         # 処理選択ラベル
         self.run_choices_pos_y = self.scroll_pos_y + self.scroll_height + 20
         self.run_choices_lab = tk.Label(text='処理選択', font=bold_font)
         self.run_choices_lab.place(relx=0.05, y=self.run_choices_pos_y)
-
         # 処理のドロップダウンメニュー
         self.process_list = ['ジャンプカット', '字幕を付ける', 'ジャンプカットして字幕を付ける']
         self.current_process = tk.StringVar(self.root)
@@ -320,29 +212,23 @@ class Videdi:
         self.process_opt.config(width=19)
         self.process_opt.place(relx=0.2, y=self.run_choices_pos_y)
         self.process_opt.config(state='disable')
-
         # オプション選択ラベル
         self.option_pos_y = self.run_choices_pos_y + 30
         self.option_lab = tk.Label(text='オプション', font=bold_font)
         self.option_lab.place(relx=0.05, y=self.option_pos_y)
-
         # ジャンプカット修正チェックボックス
         self.jumpcut_fix_bln = tk.BooleanVar()
         self.jumpcut_fix_bln.set(False)
         self.jumpcut_fix_chk = tk.Checkbutton(self.root, variable=self.jumpcut_fix_bln, text='ジャンプカット修正')
-
         # ジャンプカット動画の最小時間を設定(単位:秒)
         self.min_time = 0.5
         # ジャンプカット動画の前後の余裕を設定(単位:秒)
         self.margin_time = 0.1
-
         # 字幕修正チェックボックス
         self.subtitle_fix_bln = tk.BooleanVar()
         self.subtitle_fix_bln.set(False)
         self.subtitle_fix_chk = tk.Checkbutton(self.root, variable=self.subtitle_fix_bln, text='字幕修正')
-
         self.put_options()
-
         # 実行ボタン
         self.run_button_pos_y = self.option_pos_y + 30
         self.run_button_height = 25
@@ -351,10 +237,8 @@ class Videdi:
                                     highlightbackground=self.button_background, fg=process_button_fg, highlightthickness=0)
         self.run_button.place(relx=(1-self.run_button_relwidth)/2, y=self.run_button_pos_y,
                                   relwidth=self.run_button_relwidth, height=self.run_button_height)
-
         self.cut = False
         self.thread_event = None
-
         # メインループでイベント待ち
         if __name__ == '__main__':
             self.root.mainloop()
@@ -418,7 +302,6 @@ class Videdi:
         # 選択されたフォルダが存在しない場合
         else:
             self.current_dir_var.set('編集したい動画のあるフォルダを選択してください')
-        return
 
 # ---------------------------ファイル分割できそう(ここから) ---------------------
 
@@ -462,7 +345,6 @@ class Videdi:
         self.jumpcut_fix_chk.configure(state='disable')
         self.subtitle_fix_chk.configure(state='disable')
         self.run_button.configure(state='disable')
-        return
 
     # ボタン有効化
     def enable_all_button(self):
@@ -472,7 +354,6 @@ class Videdi:
         self.jumpcut_fix_chk.configure(state='normal')
         self.subtitle_fix_chk.configure(state='normal')
         self.run_button.configure(state='normal')
-        return
 
     # 処理の内容からオプションを表示
     def put_options(self, *args):
@@ -501,8 +382,6 @@ class Videdi:
             self.subtitle_fix_chk.configure(state=widget_state)
         else:
             self.log_frame.set_log('error:put_options method')
-            return
-        return
 
     # 処理実行ボタンの処理
     def run_button(self):
@@ -521,7 +400,6 @@ class Videdi:
             return
         # スレッディング処理を開始
         thread.start()
-        return
 
     # フォルダ内の動画をジャンプカット
     def jumpcut(self):
@@ -549,7 +427,6 @@ class Videdi:
         self.enable_all_button()
         # ジャンプカット完了ログ
         self.log_frame.set_big_log(self.process_dir_path.split('/')[-1] + 'フォルダ内の動画をジャンプカットしました')
-        return
 
 # -----------------------ファイル分割できそう(ここから)-----------------------------
     # 無音部分検出
@@ -585,6 +462,7 @@ class Videdi:
                     if 'silence_end' in words[i]:
                         # その時間を配列にfloatに変換して追加する
                         time_list.append(float(words[i + 1]))
+        # 無音部分の二次元配列に変換する
         silence_section_list = list(zip(*[iter(time_list)] * 2))
         return silence_section_list
 
@@ -675,6 +553,13 @@ class Videdi:
             command = [FFMPEG_PATH, '-i', video, '-ss', str(sections[i][0]), '-t',
                        str(sections[i][1] - sections[i][0]), split_file]
             subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # 進捗をパーセントで表示
+            if int((i + 1) * 100 / len(sections)) != int(i * 100 / len(sections)):
+                s = str(int(((i + 1) * 100) / len(sections)))
+                self.log_frame.set_log('カット処理' + '{:>3}'.format(s) + '%完了')
+        self.log_frame.set_log('カット処理完了')
+        for i in range(len(sections)):
+            split_file = jumpcut_dir + '/' + video_name + '_' + format(i+1, '0>' + str(digit)) + '.mp4'
             if self.jumpcut_fix_bln.get():
                 self.thread_event = threading.Event()
                 thread = threading.Thread(target=self.play_video_for_cut, args=[split_file, ])
@@ -690,12 +575,14 @@ class Videdi:
                 else:
                     new_sections.append(sections[i])
                 thread.join()
+                # 進捗をパーセントで表示
+                if int((i + 1) * 100 / len(sections)) != int(i * 100 / len(sections)):
+                    s = str(int(((i + 1) * 100) / len(sections)))
+                    self.log_frame.set_log('カット選択処理' + '{:>3}'.format(s) + '%完了')
             else:
                 new_sections.append(sections[i])
-            # 進捗をパーセントで表示
-            if int((i+1)*100/len(sections)) != int(i*100/len(sections)):
-                s = str(int(((i+1) * 100) / len(sections)))
-                self.log_frame.set_log('カット処理' + '{:>3}'.format(s) + '%完了')
+        if self.jumpcut_fix_bln.get():
+            self.log_frame.set_log('カット選択処理完了')
         return jumpcut_dir, new_sections
 
     # ジャンプカット修正のためのウィンドウを表示
@@ -704,14 +591,14 @@ class Videdi:
         # window = tk.Toplevel(self.root)
         window.geometry("700x550" + '+' + str(self.window_width) + '+' + str(0))
         window.title('この部分を使いますか？')
-        frame = Frame(window)
+        frame = tk.Frame(window)
         frame.pack()
-        video_audio_player = Video_Audio_player()
-        frame.video_lavel = tk.Label(window)
-        frame.video_lavel.pack()
+        player = video_audio_player.Video_Audio_player()
+        frame.video_label = tk.Label(window)
+        frame.video_label.pack()
         def end_process():
             try:
-                video_audio_player.stop()
+                player.stop()
                 window.destroy()
                 self.thread_event.set()
             except Exception as e:
@@ -748,8 +635,8 @@ class Videdi:
         leave_button.place(relx=0.51, rely=select_button_rel_y,
                                      relwidth=select_button_relwidth, height=select_button_height)
         try:
-            video_audio_player.openfile(video_path, frame.video_lavel)
-            video_audio_player.play()
+            player.openfile(video_path, frame.video_label)
+            player.play()
         except Exception as e:
             print('error:play_video_for_cut method')
             print(e)
@@ -825,11 +712,15 @@ class Videdi:
             pass
         os.mkdir('.tmp')
         subtitle_fix_bln = self.subtitle_fix_bln.get()
+        jumpcut_fix_bln = self.jumpcut_fix_bln.get()
         for i, video in enumerate(video_list):
             self.log_frame.set_log(video + 'をカットして字幕を付けます ' + str(i+1) + '/' + str(len(video_list)))
             shutil.copyfile(video, '.tmp/' + video)
             self.log_frame.set_log(video + 'の無音部分を検知します')
             cut_sections = self.silence_sections(video)
+            if len(cut_sections) == 0:
+                self.log_frame.set_log('無音部分がありませんでした')
+                continue
             video_sections = self.video_sections(cut_sections, video)
             video_sections = self.arrange_sections(video_sections, self.min_time, self.margin_time)
             if self.jumpcut_fix_bln.get():
@@ -861,6 +752,7 @@ class Videdi:
                 os.remove(jumpcut_dir + '/' + jumpcut_video)
             # 動画を結合
             self.combine_video(jumpcut_dir, video.split('.')[0])
+            self.jumpcut_fix_bln.set(jumpcut_fix_bln)
             self.subtitle_fix_bln.set(subtitle_fix_bln)
             self.log_frame.set_log(video + 'をジャンプカットして字幕を付けました')
         shutil.rmtree('.tmp')
@@ -876,14 +768,14 @@ class Videdi:
         window = tk.Toplevel()
         window.geometry("700x550"+ '+' + str(self.window_width) + '+' + str(0))
         window.title('このテキストを使いますか？')
-        frame = Frame(window)
+        frame = tk.Frame(window)
         frame.pack()
-        video_audio_player = Video_Audio_player()
-        frame.video_lavel = tk.Label(window)
-        frame.video_lavel.pack()
+        player = video_audio_player.Video_Audio_player()
+        frame.video_label = tk.Label(window)
+        frame.video_label.pack()
         def end_process():
             try:
-                video_audio_player.stop()
+                player.stop()
                 window.destroy()
                 self.thread_event.set()
             except Exception as e:
@@ -939,8 +831,8 @@ class Videdi:
         change_button.place(relx=0.79, rely=select_widget_rel_y,
                                      relwidth=select_widget_relwidth, height=select_widget_height)
         try:
-            video_audio_player.openfile(video_path, frame.video_lavel)
-            video_audio_player.play()
+            player.openfile(video_path, frame.video_label)
+            player.play()
         except Exception as e:
             print('error:play_video_for_subtitle method')
             print(e)
---
+++ video_audio_player.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import time
+import threading
+import subprocess
+import simpleaudio
+from imageio.plugins.ffmpeg import FfmpegFormat
+import imageio
+from PIL import ImageTk, Image
+
+
+APP_PATH = '/'.join(sys.argv[0].split('/')[:-3])
+# python videdi.pyで実行する時のため
+if APP_PATH == '':
+    APP_PATH = '/Applications/videdi.app'
+FFMPEG_PATH = APP_PATH + '/Contents/MacOS/ffmpeg'
+
+# 音楽を再生する
+class Audio_player():
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        try:
+            os.remove(self.audio)
+        except:
+            pass
+
+    def openfile(self, file_path):
+        self.audio = file_path.split('.')[0] + '.wav'
+        if os.path.exists(self.audio):
+            os.remove(self.audio)
+        try:
+            command = [FFMPEG_PATH, '-i', file_path, self.audio]
+            subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except Exception as e:
+            print(e)
+
+    def play(self):
+        wav_obj = simpleaudio.WaveObject.from_wave_file(self.audio)
+        self.play_obj = wav_obj.play()
+        self.play_obj.wait_done()
+
+    def stop(self):
+        try:
+            self.play_obj.stop()
+        except:
+            pass
+
+# 動画を再生する
+class Video_player():
+    def __init__(self):
+        format = FfmpegFormat(
+            "ffmpeg",
+            "Many video formats and cameras (via ffmpeg)",
+            ".mov .avi .mpg .mpeg .mp4 .mkv .wmv .webm",
+            "I",
+            )
+        imageio.formats.add_format(format,True)
+        self.stop_bln = False
+
+    def openfile(self, file_path,frame):
+        self.frame = frame
+        # try:
+        #     self.video = imageio.get_reader(file_path)
+        # except imageio.core.fetching.NeedDownloadError:
+        #     imageio.plugins.avbin.download()
+        #     self.video = imageio.get_reader(file_path)
+        self.video = imageio.get_reader(file_path)
+
+    def play(self):
+        self.video_thread = threading.Thread(target=self._stream)
+        self.video_thread.start()
+
+    def stop(self):
+        self.stop_bln = True
+
+    def _stream(self):
+        # print(self.video.get_meta_data())
+        start_time=time.time()
+        sleeptime = 1/self.video.get_meta_data()["fps"]
+        if sleeptime < 0.03:
+            sleeptime = sleeptime * 2
+        frame_now = 0
+        for i, image in enumerate(self.video.iter_data()):
+            frame_now = frame_now + 1
+            if self.stop_bln:
+                break
+            try:
+                if frame_now * sleeptime >= time.time() - start_time:
+                    img = Image.fromarray(image).resize((670, 490))
+                    frame_image = ImageTk.PhotoImage(img)
+                    self.frame.config(image=frame_image)
+                    self.frame.image = frame_image
+                    time.sleep(sleeptime)
+                else:
+                    pass
+            except:
+                pass
+
+# 動画と音声を再生する
+class Video_Audio_player():
+    def __init__(self):
+        self.video_player = Video_player()
+        self.audio_player = Audio_player()
+
+    def openfile(self, file_path, frame):
+        self.video_player.openfile(file_path, frame)
+        self.audio_player.openfile(file_path)
+
+    def play(self):
+        self.video_player.play()
+        self.audio_player.play()
+
+    def stop(self):
+        self.video_player.stop()
+        self.audio_player.stop()
